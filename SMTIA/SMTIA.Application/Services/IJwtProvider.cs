2c60fb1ed87ba9be6e1db8d4d4953062bc087a1a
Ahmet Rasim Beyhan <ahmetrasimbayhan@gmail.com>
2025-10-26 17:18:41 +0300

    Add project files.

---
+++ SMTIA/SMTIA.Application/Services/IJwtProvider.cs
@@ -0,0 +1,10 @@
+﻿using SMTIA.Application.Features.Auth.Login;
+using SMTIA.Domain.Entities;
+
+namespace SMTIA.Application.Services
+{
+    public interface IJwtProvider
+    {
+        Task<LoginCommandResponse> CreateToken(AppUser user);
+    }
+}
